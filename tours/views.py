85a4226adc9b6c89c1e69bd63bd70962d8e726ad
KUKARACHI <rahim.balabayov@gmail.com>
2020-05-04 14:27:20 +0400

    flake and pep8 reformatting

--- tours/views.py
+++ tours/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
+
 from django.views import View
-from .data  import title, subtitle, description, departures, tours
 
-
-
+from .data import departures, description, subtitle, title, tours
 
 
 def my_render(request, html, data):
@@ -18,11 +17,9 @@ def my_render(request, html, data):
 class MainView(View):
     def get(self, request, *args, **kwargs):
         chosen_tours = []
-        depart = []
         for id, tour_info in tours.items():
-            chosen_tours.append({'title': tour_info['title'], 'description': tour_info['description'], 'picture': tour_info['picture'], 'id': str(id)})
-
-
+            chosen_tours.append({'title': tour_info['title'], 'description': tour_info['description'],
+                                 'picture': tour_info['picture'], 'id': str(id)})
 
         return my_render(request, 'tours/index.html', {'tours': chosen_tours})
 
@@ -35,9 +32,9 @@ class DepartureView(View):
         max_price = 0
         min_nights = int(1e10)
         max_nights = 0
-        for id, tour_info in tours.items():
+        for tour_id, tour_info in tours.items():
             if tour_info['departure'] == departure:
-                chosen_tours.append((str(id), tour_info))
+                chosen_tours.append((str(tour_id), tour_info))
                 min_price = min(min_price, tour_info['price'])
                 max_price = max(max_price, tour_info['price'])
                 min_nights = min(min_nights, tour_info['nights'])
@@ -46,9 +43,9 @@ class DepartureView(View):
         departure = departures[departure].split()[1]
 
         return my_render(request, 'tours/departure.html', {'min_nights': min_nights, 'max_nights': max_nights,
-                                                        'min_price': min_price, 'max_price': max_price,
-                                                        'chosen_tours': chosen_tours, 'count': len(chosen_tours),
-                                                           'departure': departure })
+                                                           'min_price': min_price, 'max_price': max_price,
+                                                           'chosen_tours': chosen_tours, 'count': len(chosen_tours),
+                                                           'departure': departure})
 
 
 class TourView(View):
